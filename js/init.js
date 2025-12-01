4d576a1adf5d7619565f19420437775495a92cc7
Onkar Vatsa <onkarvatsa@gmail.com>
2025-12-01 19:01:24 +0530

    update

--- js/init.js
+++ js/init.js
@@ -542,7 +542,7 @@ function arlo_tm_animate_text() {
   var animateSpan = jQuery(".arlo_tm_animation_text_word");
 
   animateSpan.typed({
-    strings: ["Full Stack Web Developer", "Canva Designer", "Freelancer"],
+    strings: ["Full Stack Web Developer", "Canva Designer",],
     loop: true,
     startDelay: 1e3,
     backDelay: 2e3,
